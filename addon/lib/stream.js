d174b790e9667a2a742395b0d8f8ce8180166eb7
Eric van Blokland <eric@footsteps.nl>
2016-11-28 17:39:28 +0100

    Streams package include is now based on Ember version (2.6 vs 2.7)

--- addon/lib/stream.js
+++ addon/lib/stream.js
@@ -6,7 +6,14 @@ streamification of Ember's view layer happened.
 
 https://github.com/emberjs/ember.js/pull/9693 is pending to expose them (hopefully in 1.10).
 */
-var Stream = Ember.__loader.require('ember-metal/streams/stream')['Stream'];
+
+var streamPackage='ember-metal';
+
+if(Ember.VERSION.split('.')[1]>6) {
+	streamPackage='ember-htmlbars';
+} 
+
+var Stream = Ember.__loader.require(streamPackage+'/streams/stream')['Stream'];
 
 var createStream=function(fn) {
 	return new Stream(fn);
@@ -14,6 +21,6 @@ var createStream=function(fn) {
 
 
 
-export var readArray = Ember.__loader.require('ember-metal/streams/utils')['readArray'];
-export var read = Ember.__loader.require('ember-metal/streams/utils')['read'];
+export var readArray = Ember.__loader.require(streamPackage+'/streams/utils')['readArray'];
+export var read = Ember.__loader.require(streamPackage+'/streams/utils')['read'];
 export default createStream;
