b34840122da8c342b785c113aef0d98d8f1eaa60
Eric van Blokland <eric@footsteps.nl>
2017-05-12 12:43:20 +0200

    Removed accidental addition of ComponentLookup override

--- app/initializers/furnace-i18n.js
+++ app/initializers/furnace-i18n.js
@@ -7,26 +7,15 @@ import Service from 'furnace-i18n/services/i18n';
 export function initialize(application) {
 	
 	application.register('service:i18n', Service);
-  
+
 	application.register('i18n:translate', I18nTranslate);
 	application.register('i18n:stream', I18nStream);
-  
+
 	application.inject('route', 'i18nText', 'i18n:translate');
 	application.inject('model', 'i18nText', 'i18n:translate');
 	application.inject('component', 'i18nText', 'i18n:translate');
 	application.inject('controller', 'i18nText', 'i18n:translate');
 	application.inject('validator', 'i18nText', 'i18n:translate');
-	
-	Ember.ComponentLookup.reopen({
-		componentFor(name, owner, options) {
-			var fullName = 'component:' + name;
-			return owner._lookupFactory(fullName, options);
-		},
-		layoutFor(name, owner, options) {			
-			 var templateFullName = 'template:components/' + name;
-			 return owner.lookup(templateFullName, options);
-		}
-	});
 };
 
 export default {
